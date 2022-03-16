59bab47bbea594ea4edbcee83a4ec99af27f9887
huynhkimloan <1951052102loan@ou.edu.vn>
2022-03-16 09:01:49 +0700

    eglishapp

---
+++ EnglishApp/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.hkl.englishapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.base;
+    requires java.sql;
+
+    opens com.hkl.englishapp to javafx.fxml;
+    exports com.hkl.englishapp;
+}
